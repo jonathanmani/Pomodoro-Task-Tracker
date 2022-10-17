c791d3a97a3ca944da04c2cfb95c860a55cc8581
Jonathan Mani <39526978+jonathanmani@users.noreply.github.com>
2022-10-16 20:47:06 -0400

    added new routes

--- routes/todos.js
+++ routes/todos.js
@@ -1,16 +1,15 @@
-const express = require('express')
-const router = express.Router()
-const todosController = require('../controllers/todos') 
-const { ensureAuth } = require('../middleware/auth')
+const express = require("express");
+const router = express.Router();
+const todosController = require("../controllers/todos");
+const { ensureAuth } = require("../middleware/auth");
 
-router.get('/', ensureAuth, todosController.getTodos)
+router.get("/", ensureAuth, todosController.getTodos);
+router.post("/createTodo", todosController.createTodo);
+router.put("/markComplete", todosController.markComplete);
+router.put("/markIncomplete", todosController.markIncomplete);
+router.put("/upTodo", todosController.upTodo);
+router.put("/downTodo", todosController.downTodo);
+router.put("/editTodo", todosController.editTodo);
+router.delete("/deleteTodo", todosController.deleteTodo);
 
-router.post('/createTodo', todosController.createTodo)
-
-router.put('/markComplete', todosController.markComplete)
-
-router.put('/markIncomplete', todosController.markIncomplete)
-
-router.delete('/deleteTodo', todosController.deleteTodo)
-
-module.exports = router
+module.exports = router;
